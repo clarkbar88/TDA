fa2aefbcf7b03724e2f9b963503a59102a3875a9
clarkbar88 <timothy.clark@kingcounty.gov>
2019-07-31 13:46:02 -0700

    NAMESPACE update

---
+++ R/TDA.R
@@ -0,0 +1,9 @@
+#' TDA: (Trend Detection Analysis) some functions for implement McBride 2018
+#'
+#' TDA provides two function for implementing the trend detection analysis tool described in McBride 2018
+#' 
+#' @import EnvStats
+#' @importFrom plyr adply
+#' @import tidyverse
+#' @import lubridate
+"_PACKAGE"
