df6ceb92416e93828f932f38c83b0e70e805cb48
Tim Clark <34457830+clarkbar88@users.noreply.github.com>
2019-08-01 11:20:22 -0700

    Update TDA.R

--- R/TDA.R
+++ R/TDA.R
@@ -1,6 +1,6 @@
-#' TDA: (Trend Detection Analysis) some functions for implement McBride 2018
+#' TDA: (Trend Detection Analysis) some functions for implement McBride 2019
 #'
-#' TDA provides two function for implementing the trend detection analysis tool described in McBride 2018
+#' TDA provides two function for implementing the trend detection analysis tool described in McBride 2019
 #' @importFrom magrittr %>%
 #' @import purrr
 #' @import dplyr
